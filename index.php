7e4a7ecf8ecf96e638fe95bb83662b406f6d9c58
MPalamut <palamuto@live.de>
2025-09-02 15:39:38 +0200

    ok

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
       </form>
     </div>
       <footer class="bg-gray-800 text-gray-100 p-4 h-32">   
-         <?php echo "Copyright 2025" ?>
+         <?php echo "Copyright proflee 2025" ?>
       </footer>
   </section>
 </body>
