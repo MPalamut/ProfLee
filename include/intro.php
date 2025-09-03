aa71503316873f982902111359048dd8632e76b5
MPalamut <palamuto@live.de>
2025-09-03 13:56:37 +0200

    ok

--- include/intro.php
+++ include/intro.php
@@ -1,6 +1,6 @@
 <div class="bg-gradient-to-r from-zinc-400 to-slate-500 p-10 flex items-center gap-5">
          <img class="h-32 w-32 rounded-full shadow-md shadow-black" src="assets/images/sunset.jpg" alt="">
-         <div><span class="text-3xl">Welcome profLee</span>
+         <div><span class="text-3xl">Welcome proflee</span>
             <p class="text-gray-100">My Portfolio</p>
          </div>
       </div>
